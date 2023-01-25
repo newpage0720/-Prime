3052d950f57836f0c90c45d3b636f76c8054b761
GR <newpage0720@gmail.com>
2023-01-25 09:31:12 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個大於等於2的正整數:");
+            int.TryParse(Console.ReadLine(), out int num);
+            int i = 2;
+
+            if (num >= 2)
+            {
+                while (i < num)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine($"{num}不是質數");
+                        break;
+                    }
+                    i++;
+                }
+                if (i == num)
+                {
+                    Console.WriteLine($"{num}是質數");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("輸入的字不是大於等於2的正整數");
+            }
+        }
+    }
+}
