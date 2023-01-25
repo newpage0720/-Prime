293746d3fd05fae892de143e8ecc592977e5830b
GR <newpage0720@gmail.com>
2023-01-25 09:08:53 +0800

    test

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個整數:");
+            int.TryParse(Console.ReadLine(),out int num);
+            int i = 2;
+
+            if (num >= 2)
+            {
+                while (i < num)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine("不是質數");
+                        break;
+                    }
+                    i++;
+                }
+                if (i == num)
+                {
+                    Console.WriteLine("是質數");
+                }
+            }
+            else
+            {
+                Console.WriteLine("請輸入大於2的正整數!");
+            }
+        }
+    }
+}
+
+
